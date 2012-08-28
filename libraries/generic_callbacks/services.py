3b117b7734877b5c336cfb8a98901082a900382f
Guillaume 'Llew' Delpierre <gde@llew.me>
2012-08-28 12:02:22 +0200

    services.srvctl now returns a tupple containing the result of the command

--- libraries/generic_callbacks/services.py
+++ libraries/generic_callbacks/services.py
@@ -1,6 +1,16 @@
+#! /usr/bin/env python
+# -*- coding: iso-latin-1 -*-
 """
 Generic callbacks for services
 """
+
+__docformat__ = "restructuredtext en"
+__author__ = ["Denis 'jawa' Pompilio", "Guillaume 'Llew' Delpierre"]
+__credits__ = "Denis 'jawa' Pompilio"
+__license__ = "GPLv3"
+__maintainer__ = ["Denis 'jawa' Pompilio", "Guillaume 'Llew' Delpierre"]
+__email__ = ["denis.pompilio@gmail.com", "gde@llew.me"]
+__status__ = "Development"
 
 # we import as private
 import sc_logs as __LOG
