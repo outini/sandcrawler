dc8809a277403bc68e4d27b090c7ae8ee34237d4
Denis 'jawa' Pompilio <denis.pompilio@gmail.com>
2012-08-28 19:14:42 +0200

    Major changes in callbacks system. Now callbacks have trk (trunk) and mom (parent class) for quick access to ressources. Self pointeur design itself again and no more the trunk class (server).
    Subcallbacks are now availables and fully functionnals.
    
    A FAPI wrapper has been implemented in server class to make direct call to fabric_wrp
    without the ipaddress argument. This argument is filled with server IP.

--- libraries/sc_systems.py
+++ libraries/sc_systems.py
@@ -16,12 +16,14 @@ import types
 import collections
 import sc_logs as LOG
 import sc_common as scc
+import fabric_wrp as fapi
 
 class CallBacks():
     """ class to store callbacks """
-    def __init__(self, parent, module):
+    def __init__(self, trunk, parent, module):
         LOG.log_d("initialising Callbacks class %s" % (module))
-        self.__parent = parent
+        self.trk = trunk
+        self.mom = parent
         modlist = list()
 
         # replace OS type by generic prefix in module name
@@ -70,19 +72,26 @@ class CallBacks():
                 if attr[0:2] == "__":
                     continue
 
-                # callback store exemple:
-                #print "callback store: self.%s --> %s.%s" % (
-                #    attr, self.calls.__name__, attr)
-
-                # store callback, if callable self refer to the parent
                 module_attr = getattr(mod, attr)
+                ## store callback, if callable self refer to the callback
                 if isinstance(module_attr, collections.Callable):
                     setattr(self, attr,
-                            types.MethodType(module_attr, self.__parent))
+                            types.MethodType(module_attr, self))
                 else:
                     setattr(self, attr, module_attr)
 
         return True
+
+def fapi_wrapper(method_name):
+    """ trying metaclass """
+    def _method(self, *argl, **argd):
+        # adding self.srv_ip as first argument for every fapi invocations
+        # all the fabric_wrp methods MUST take the ip as first argument
+        argl = list(argl)
+        argl.insert(0, self.srv_ip)
+        argl = tuple(argl)
+        return getattr(fapi, '%s' % method_name)(*argl,**argd)
+    return _method
 
 class Server:
     """ server class with os/distribs callbacks """
@@ -93,27 +102,33 @@ class Server:
         self.hostname = None
         self.systemtype = systemtype
 
+        self.fapi = scc.AttStr('fapi calls wrapper')
+        self.load_fapi()
+
         if load:
             LOG.log_d("guessing server's system type")
             self.guess_system(systemtype)
+
 
     def is_up(self, port = 22):
         """ check if host is UP """
         return scc.is_host_up(self.srv_ip, int(port))
 
-    def __getcontainer(self, container):
-        """ get container from container name """
-        splitted = container.split('.')
+
+    def __getparent(self, callback_name):
+        """ get parent from callback name """
+        splitted = callback_name.split('.')
         if len(splitted) == 1:
             return None
 
-        container = self
+        parent = self
         for member in splitted[:-1]:
-            if not hasattr(container, member):
-                container.load_callbacks(member)
-            container = getattr(container, member)
+            if not hasattr(parent, member):
+                parent.load_callbacks(member)
+            parent = getattr(parent, member)
 
-        return container
+        return parent
+
 
     def load_callbacks(self, callback):
         """ callbacks loader """
@@ -122,14 +137,38 @@ class Server:
 
         # load callback specified by arg, split by dot and recurse load
         callback_name = callback.split('.')[-1]
-        container = self.__getcontainer(callback)
-        if container is None:
-            container = self
+        parent = self.__getparent(callback)
+        if parent is None or not hasattr(parent, 'trk'):
+            trunk = parent = self
+        else:
+            trunk = parent.trk
 
         target_module = "%s.%s" % (self.systemtype, callback)
-        setattr(container, callback_name, CallBacks(self, target_module))
+        setattr(parent, callback_name, CallBacks(trunk, parent,
+                                                 target_module))
 
         return True
+
+
+    def load_fapi(self):
+        """
+        wrap methods from fabric_wrp module
+        wrapper assume that the first argument of each fapi methods is srv_ip
+        all invocations have to be done without the first argument (srv_ip)
+        """
+        for attr in dir(fapi):
+            if attr[0:2] == "__":
+                continue
+
+            module_attr = getattr(fapi, attr)
+            if isinstance(module_attr, collections.Callable):
+                setattr(self.fapi, attr,
+                        types.MethodType(fapi_wrapper(attr), self))
+            else:
+                setattr(self, attr, module_attr)
+
+        return True
+
 
     def guess_system(self, systype=None):
         """ guess on system of remote target """
